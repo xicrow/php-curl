39f29309b2343be8a2295db3c702caf97144a995
Jan Ebsen <xicrow@gmail.com>
2018-10-05 23:28:20 +0200

    Update demo

--- demo/index.php
+++ demo/index.php
@@ -13,7 +13,11 @@ $urls = [
     'http://jsonplaceholder.typicode.com/todos/5',
 ];
 
-// List of requests
+/**
+ * List of requests
+ *
+ * @var Request[] $requests
+ */
 $requests = [];
 foreach ($urls as $urlIndex => $url) {
     $requests[] = new Request([CURLOPT_URL => $url]);
@@ -22,94 +26,99 @@ foreach ($urls as $urlIndex => $url) {
 // -------------------------------------------------------------------------------------------------
 // Single requests
 // -------------------------------------------------------------------------------------------------
-//$request  = current($requests);
-//$response = $request->execute();
-//echo '<pre>' . print_r($request, true) . '</pre>';
-//echo '<pre>' . print_r($response, true) . '</pre>';
+if (false) {
+    $request  = current($requests);
+    $response = $request->execute();
+    echo '<pre>' . print_r($request, true) . '</pre>';
+    echo '<pre>' . print_r($response, true) . '</pre>';
+}
 
 // -------------------------------------------------------------------------------------------------
 // Batch requests
 // -------------------------------------------------------------------------------------------------
-//$batch = new Batch();
-//$batch->setOptions([
-//    'max_concurrent_requests' => 5,
-//]);
-//$batch->addRequests($requests);
-//$batch->setCurlOptions([
-//    CURLOPT_CUSTOMREQUEST  => 'GET',
-//    CURLOPT_URL            => '',
-//    CURLOPT_PORT           => 80,
-//    CURLOPT_POSTFIELDS     => [],
-//    CURLOPT_TIMEOUT        => 5,
-//    CURLOPT_HEADER         => true,
-//    CURLOPT_NOBODY         => false,
-//    CURLOPT_RETURNTRANSFER => true,
-//    CURLOPT_FOLLOWLOCATION => true,
-//    CURLOPT_MAXREDIRS      => 3,
-//    CURLOPT_USERAGENT      => 'Mozilla/4.0',
-//]);
-//$batch->execute();
-//foreach ($batch->getRequests() as $requestIndex => $request) {
-//    foreach ($batch->getResponses() as $responseIndex => $response) {
-//        if ($requestIndex != $responseIndex) {
-//            continue;
-//        }
-//
-//        echo '<pre>';
-//        print_r([
-//            //            '$request'         => $request,
-//            //            '$response'        => $response,
-//            'url'              => $request->getCurlOption(CURLOPT_URL),
-//            //            'headers'          => $response->getHeaders(),
-//            'http_status_code' => $response->getHttpStatusCode(),
-//            'content_type'     => $response->getContentType(),
-//            //            'body'             => $response->getBody(),
-//            'total_time'       => $response->getInfo()['total_time'],
-//        ]);
-//        echo '</pre>';
-//    }
-//}
+if (false) {
+    $batch = new Batch([
+        'max_concurrent_requests' => 5,
+    ]);
+    $batch->addRequests($requests);
+    $batch->curlOptions()->set([
+        CURLOPT_CUSTOMREQUEST  => 'GET',
+        CURLOPT_URL            => '',
+        CURLOPT_PORT           => 80,
+        CURLOPT_POSTFIELDS     => [],
+        CURLOPT_TIMEOUT        => 5,
+        CURLOPT_HEADER         => true,
+        CURLOPT_NOBODY         => false,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_MAXREDIRS      => 3,
+        CURLOPT_USERAGENT      => 'Mozilla/4.0',
+    ]);
+    $batch->execute();
+    foreach ($batch->getRequests() as $requestIndex => $request) {
+        foreach ($batch->getResponses() as $responseIndex => $response) {
+            if ($requestIndex != $responseIndex) {
+                continue;
+            }
+
+            echo '<pre>';
+            print_r([
+                //            '$request'         => $request,
+                //            '$response'        => $response,
+                'url'              => $request->curlOptions()->get(CURLOPT_URL),
+                //                            'headers'          => $response->headers(),
+                'http_status_code' => $response->headers()->getHttpStatusCode(),
+                'content_type'     => $response->headers()->getContentType(),
+                //                            'body'             => $response->body(),
+                'total_time'       => $response->info('total_time'),
+            ]);
+            echo '</pre>';
+        }
+    }
+}
 
 // -------------------------------------------------------------------------------------------------
 // Single requests vs. batch requests
 // -------------------------------------------------------------------------------------------------
-echo '<pre>';
-echo 'Single requests';
-echo "\n";
-$timeStart = microtime(true);
-foreach ($requests as $request) {
-    $response = $request->execute();
-    echo '#' . str_pad(($urlIndex + 1), (strlen(count($urls))), '0', STR_PAD_LEFT);
-    echo ': ';
-    echo $response->getHeader('Http-Version') . ' ' . $response->getHeader('Http-Status-Code') . ' ' . $response->getHeader('Http-Status-Message');
-    echo ' ';
-    echo $request->getCurlOption(CURLOPT_URL);
+if (true) {
+    echo '<pre>';
+    echo 'Single requests';
     echo "\n";
-}
-$timeStop = microtime(true);
-echo 'Elapsed time: ' . round($timeStop - $timeStart, 4) . ' sec.';
-echo '</pre>';
+    $timeStart = microtime(true);
+    foreach ($requests as $requestIndex => $request) {
+        $response = $request->execute();
+        echo '#' . str_pad(($requestIndex + 1), (strlen(count($requests))), '0', STR_PAD_LEFT);
+        echo ': ';
+        echo $response->headers()->getHttpVersion() . ' ' . $response->headers()->getHttpStatusCode() . ' ' . $response->headers()->getHttpStatusMessage();
+        echo ' ';
+        echo $request->curlOptions()->get(CURLOPT_URL);
+        echo "\n";
+    }
+    $timeStop = microtime(true);
+    echo 'Elapsed time: ' . round($timeStop - $timeStart, 4) . ' sec.';
+    echo '</pre>';
 
-echo '<pre>';
-echo 'Batch requests';
-echo "\n";
-$timeStart = microtime(true);
-$batch     = new Batch(['max_concurrent_requests' => 10]);
-$batch->addRequests($requests);
-$batchErrors = $batch->execute();
-foreach ($batch->getResponses() as $responseIndex => $response) {
-    $request = $requests[$responseIndex];
-    echo '#' . str_pad(($responseIndex + 1), (strlen(count($urls))), '0', STR_PAD_LEFT);
-    echo ': ';
-    echo $response->getHeader('Http-Version') . ' ' . $response->getHeader('Http-Status-Code') . ' ' . $response->getHeader('Http-Status-Message');
-    echo ' ';
-    echo $request->getCurlOption(CURLOPT_URL);
+    echo '<pre>';
+    echo 'Batch requests';
     echo "\n";
+    $timeStart = microtime(true);
+    $batch     = new Batch(['max_concurrent_requests' => 10]);
+    $batch->addRequests($requests);
+    $batchErrors = $batch->execute();
+    foreach ($batch->getResponses() as $responseIndex => $response) {
+        $request = $requests[$responseIndex];
+        echo '#' . str_pad(($responseIndex + 1), (strlen(count($requests))), '0', STR_PAD_LEFT);
+        echo ': ';
+        echo $response->headers()->getHttpVersion() . ' ' . $response->headers()->getHttpStatusCode() . ' ' . $response->headers()->getHttpStatusMessage();
+        echo ' ';
+        echo $request->curlOptions()->get(CURLOPT_URL);
+        echo "\n";
+    }
+    if (!empty($batchErrors)) {
+        echo '$batchErrors = ' . print_r($batchErrors, true);
+        echo "\n";
+    }
+    $timeStop = microtime(true);
+    echo 'Elapsed time: ' . round($timeStop - $timeStart, 4) . ' sec.';
+    echo '</pre>';
 }
-if (!empty($batchErrors)) {
-    echo '$batchErrors = ' . print_r($batchErrors, true);
-    echo "\n";
-}
-$timeStop = microtime(true);
-echo 'Elapsed time: ' . round($timeStop - $timeStart, 4) . ' sec.';
-echo '</pre>';
